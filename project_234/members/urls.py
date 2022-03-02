f5228744812841a8188e1bf2eea5cfd624b3fb0a
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-03-03 03:33:32 +0600

    user edit option added

--- project_234/members/urls.py
+++ project_234/members/urls.py
@@ -6,5 +6,6 @@ from .views import *
 urlpatterns = [
     
     path('register/',UserRegisterView.as_view(),name='register'),
+     path('edit_profile/',UserEditView.as_view(),name='edit_profile'),
 
 ]
--- project_234/members/views.py
+++ project_234/members/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm ,UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
