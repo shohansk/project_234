129e165ce07b8757724f997837d1b5d9dcff43d9
Abdul Alim Shohan <shohankhansk111@gmail.com>
2022-03-21 03:18:41 +0600

    user profile section with image

--- project_234/theblog/models.py
+++ project_234/theblog/models.py
@@ -2,6 +2,7 @@
 
 
 from ast import mod
+import profile
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -54,6 +55,13 @@ class Comment(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(upload_to = "files/profile_pic",null = True)
+    website_link = models.CharField( max_length=250,null=True,blank=True)
+    facebook_link = models.CharField( max_length=250,null=True,blank=True)
+    youtube_link = models.CharField( max_length=250,null=True,blank=True)
+    instagram_link = models.CharField( max_length=250,null=True,blank=True)
+    twitter_link = models.CharField( max_length=250,null=True,blank=True)
+    reddit_link = models.CharField( max_length=250,null=True,blank=True)
 
     def __str__(self):
         return str(self.user)
